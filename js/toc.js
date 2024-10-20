d81fa079823558525f4bb6d93858685b5744894f
rmrt1n <rmrt1n@users.noreply.github.com>
2024-10-20 16:19:11 +0000

    deploy: 9a50b561b9ba1ed54becf67a8fd0123ca0c7e414

--- js/toc.js
+++ js/toc.js
@@ -3,10 +3,10 @@ const toc = document.querySelector('.toc details')
 
 if (window.matchMedia(mediaQuery).matches) toc.open = true
 
-window.matchMedia(mediaQuery).addEventListener((e) => {
+window.matchMedia(mediaQuery).addEventListener('change', (e) => {
   if (e.matches) toc.open = true
 })
 
-window.matchMedia('not all and ' + mediaQuery).addEventListener((e) => {
+window.matchMedia('not all and ' + mediaQuery).addEventListener('change', (e) => {
   if (e.matches) toc.open = false
 })
