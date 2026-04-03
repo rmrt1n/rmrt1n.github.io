50b96d29dc9c81fac9b7c4e5f955f6c5fa7a2f18
rmrt1n <rmrt1n@users.noreply.github.com>
2026-04-03 18:40:59 +0000

    deploy: 4415f64f14ff44b6b45e209662168c64adad2717

--- flag/sw.js
+++ flag/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'rmctf-'
-const CACHE_VERSION = 2 // NOTE: bump this version every update
+const CACHE_VERSION = 1 // NOTE: toggle this version every update to cache deps (0/1)
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION
 
 self.addEventListener('install', (event) => {
@@ -9,7 +9,7 @@ self.addEventListener('install', (event) => {
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll([
         '/flag/',
-        '/iFZeBssXUJ.css',
+        '/QXOtk5W6wg.css',
         '/favicon.png',
         '/manifest.json',
       ])
